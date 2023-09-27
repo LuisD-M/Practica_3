15ce8e09e9aee72c06dd66906ed07216e2367932
Luis Muñoz <luisd.munoz@udea.edu.co>
2023-09-26 22:20:36 -0500

    Aplicacion Menu y Accseso a administrador

--- Practica_3/main.cpp
+++ Practica_3/main.cpp
@@ -9,41 +9,90 @@ using namespace std;
 
 int tamtex(string archivoleer);
 void binario(int tamaño, char *catexto, int *texbinario, string archivoleer);
-void metodo1(int tamaño, int semilla, int *texbinario, string nombre);
+void metodo1(int tamaño, int semilla, int *texbinario, int *codificado, string nombre);
 void metodo2(int tamaño, int semilla, int *texbinario, int *codificado, string nombre);
 void deco1(int tamaño, int semilla, int *texbinario, string nombre, string nombredeco);
 void deco2(int tamaño, int semilla, int *texbinario, int *codificado, string nombre, string nombredeco);
+int menu();
 
 
 int main()
 {
-    int tamaño, semilla, metodo;
-    string archivoleer, nombre, nombredeco;
+    int tamaño, semilla, metodo, opcion=10, numerousu;
+    string archivoleer, nombre, nombredeco, clave, linea, claveusu, datos;
     cout<<"Ingrese la semilla n: "; cin>>semilla;
     cout<<"Ingrese el metodo de codificacion (1-2): "; cin>>metodo;
-    cout<<"Ingrese el nombre del archivo a leer: "; cin>>archivoleer;
-    cout<<"Ingrese el nombre del archivo codificado: "; cin>>nombre;
-    cout<<"Ingrese el nombre del archivo decodificado: "; cin>>nombredeco;
+
+    ifstream in;
+    ofstream on;
 
 
-    tamaño=tamtex(archivoleer);                                                                     //Calcular el tamaño del texto.
+    while(opcion!=0){
+        cout<<endl;
+        menu();
+        cout<<"Ingrese la opcion deseada: "; cin>>opcion;
 
-    char catexto[tamaño];
-    int texbinario[8*tamaño];
-    binario(tamaño, &catexto[0], &texbinario[0], archivoleer);                                       //Genera 2 cadenas, caracteres y binario.
-    int codificado[8*tamaño];
+        if(opcion==1){
+            cout<<"Ingrese el nombre del archivo que tiene la clave incriptada: "; cin>>nombre;
+            tamaño=tamtex(nombre);
+            char catexto[tamaño/8];
+            int texbinario[tamaño];
+            int codificado[tamaño];
 
-    if(metodo==1){
-        metodo1(tamaño, semilla, &texbinario[0],nombre);
-        deco1(tamaño, semilla, &texbinario[0],nombre,nombredeco);
+            cout<<"Ingrese el nombre del archivo que va a tener la clave desencriptada: "; cin>>nombredeco;
+            if(metodo==1) deco1(tamaño/8, semilla, &texbinario[0], nombre, nombredeco);
+            else deco2(tamaño/8, semilla, &texbinario[0], &codificado[0], nombre, nombredeco);
+
+            in.open(nombredeco);
+
+            if(in.fail()){
+                cout<<"El archivo texto de la clave decodificada no se pudo abrir."<<endl;
+                exit(1); }
+
+            while (getline(in, linea))
+                clave += linea;
+
+            in.close();
+
+            cout<<"Ingrese la clave de administrador: "; cin>>claveusu;
+
+            if(claveusu == clave){
+                cout<<"Administradro en linea."<<endl;
+                cout<<"Ingrese el nombre del archivo de los usuarios: "; cin>>archivoleer;
+
+                on.open(archivoleer,ios::app);
+                if(on.fail()){
+                    cout<<"El archivo texto de los usuarios no se pudo abrir."<<endl;
+                    exit(1); }
+
+                cout<<"Ingrese el numero de usuarios que desea registrar: "; cin>>numerousu;
+
+                while(numerousu!=0){
+                    cout<<"Ingrese los datos del usuario nuevo cedula,clave,dinero: "; cin>>datos;
+                    on<<datos<<endl;
+                    numerousu--;
+                }
+                on.close();
+
+            }
+            else cout<<"Las claves no coinciden, no puede ingresar"<<endl;
+
+        }
+
+
+
+
+
     }
-    else if(metodo==2){
-        metodo2(tamaño, semilla, &texbinario[0], &codificado[0],nombre);
-        deco2(tamaño, semilla, &texbinario[0], &codificado[0], nombre, nombredeco);
-    }
-    else cout<<"El metodo ingresado es invalido."<<endl;
 }
 
+int menu(){
+    cout<<"-------------------- Menu de la aplicacion --------------------"<<endl;
+    cout<<" 1. Registrar usuarios. "<<endl;
+    cout<<" 2. Consultar saldo."<<endl;
+    cout<<" 3. Retirar dinero."<<endl;
+    cout<<" 0. Para finalizar."<<endl;
+}
 
 int tamtex(string archivoleer){
     int tamaño;
@@ -95,24 +144,16 @@ void binario(int tamaño, char *catexto, int *texbinario, string archivoleer){
         texbinario[i]=vect[i];
 }
 
-void metodo1(int tamaño, int semilla, int *texbinario, string nombre){
+void metodo1(int tamaño, int semilla, int *texbinario, int *codificado, string nombre){
     int cont0=0, cont1=0, aux=0;
-
-    ofstream archivo;
-    archivo.open(nombre);
-
-    if(archivo.fail()){
-        cout<<"El archivo Codificado del metodo 1 no ce pudo crear."<<endl;
-        exit(1);
-    }
 
     for(int i=0; i<8*(tamaño); i=i+semilla){
 
         if(cont0 == cont1){                                            //el # de 1nos es igaul al # de 0ros.
             for(int j=i; j<i+semilla; j++){
-                if(texbinario[j]==1) archivo<<0;
+                if(texbinario[j]==1) codificado[j]=0;
                 else
-                    archivo<<1;
+                    codificado[j]=1;
             }
         }
 
@@ -120,12 +161,12 @@ void metodo1(int tamaño, int semilla, int *texbinario, string nombre){
             aux=0;
             for(int j=i; j<i+semilla; j++){
                 if(aux==1){                                              //Invierte el 2 bit
-                    if(texbinario[j]==1) archivo<<0;
-                    else archivo<<1;
+                    if(texbinario[j]==1) codificado[j]=0;
+                    else codificado[j]=1;
                     aux=0;
                 }
                 else{
-                    archivo<<texbinario[j];
+                    codificado[j]=texbinario[j];
                     aux++;
                 }
             }
@@ -135,12 +176,12 @@ void metodo1(int tamaño, int semilla, int *texbinario, string nombre){
             aux=0;
             for(int j=i; j<i+semilla; j++){
                 if(aux==2){
-                    if(texbinario[j]==1) archivo<<0;
-                    else archivo<<1;
+                    if(texbinario[j]==1) codificado[j]=0;
+                    else codificado[j]=1;
                     aux=0;
                 }
                 else{
-                    archivo<<texbinario[j];
+                    codificado[j]=texbinario[j];
                     aux++;
                 }
             }
@@ -153,6 +194,18 @@ void metodo1(int tamaño, int semilla, int *texbinario, string nombre){
             else cont0++;
         }
     }
+
+    ofstream archivo;
+    archivo.open(nombre);
+
+    if(archivo.fail()){
+        cout<<"El archivo Codificado del metodo 1 no ce pudo crear."<<endl;
+        exit(1);
+    }
+
+    for(int i=0; i<8*tamaño; i++)
+        archivo<<codificado[i];
+
     archivo.close();
 }
 
