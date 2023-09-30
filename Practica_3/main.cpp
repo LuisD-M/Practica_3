a74b999ddfdba96bc3c527be11b84a8ec80e3750
Luis Muñoz <luisd.munoz@udea.edu.co>
2023-09-29 22:41:48 -0500

    Opciones 2 y 3 de la aplicacion

--- Practica_3/main.cpp
+++ Practica_3/main.cpp
@@ -3,25 +3,41 @@
 #include <string.h>
 #include <stdlib.h>
 #include <bitset>
+#include <sstream>
 #include <vector>
 
 using namespace std;
 
 int tamtex(string archivoleer);
 void binario(int tamaño, char *catexto, int *texbinario, string archivoleer);
+void binAcadena(int tamaño, int *texbinario, string archivoleer);
 void metodo1(int tamaño, int semilla, int *texbinario, int *codificado, string nombre);
 void metodo2(int tamaño, int semilla, int *texbinario, int *codificado, string nombre);
 void deco1(int tamaño, int semilla, int *texbinario, string nombre, string nombredeco);
 void deco2(int tamaño, int semilla, int *texbinario, int *codificado, string nombre, string nombredeco);
 int menu();
 
+struct Usuario {
+    string cedula;
+    string nombre;
+    int dinero;
+};
+
 
 int main()
 {
-    int tamaño, semilla, metodo, opcion=10, numerousu;
-    string archivoleer, nombre, nombredeco, clave, linea, claveusu, datos;
+    int tamaño, tamaño1, semilla, metodo, opcion=10, numerousu, nuevodinero, dineroretirar;
+    string archivousuarios, nombre, nombredeco, clave, linea, claveusu, datos, cedula_ingre;
+    string cedula, nombre1, dinero_str;
+    vector<Usuario> usuarios;
+    bool cedulaEncontrada = false;
+
     cout<<"Ingrese la semilla n: "; cin>>semilla;
     cout<<"Ingrese el metodo de codificacion (1-2): "; cin>>metodo;
+    cout<<"Ingrese el nombre del archivo de los usuarios: "; cin>>archivousuarios;
+
+    cout<<"Ingrese el nombre del archivo que tiene la clave incriptada: "; cin>>nombre;
+    cout<<"Ingrese el nombre del archivo que va a tener la clave desencriptada: "; cin>>nombredeco;
 
     ifstream in;
     ofstream on;
@@ -33,13 +49,12 @@ int main()
         cout<<"Ingrese la opcion deseada: "; cin>>opcion;
 
         if(opcion==1){
-            cout<<"Ingrese el nombre del archivo que tiene la clave incriptada: "; cin>>nombre;
             tamaño=tamtex(nombre);
-            char catexto[tamaño/8];
-            int texbinario[tamaño];
-            int codificado[tamaño];
+            tamaño1=tamtex(archivousuarios);
+            int texbinario[tamaño], texbinario1[tamaño1];
+            int codificado[tamaño], codificado1[tamaño1];
 
-            cout<<"Ingrese el nombre del archivo que va a tener la clave desencriptada: "; cin>>nombredeco;
+
             if(metodo==1) deco1(tamaño/8, semilla, &texbinario[0], nombre, nombredeco);
             else deco2(tamaño/8, semilla, &texbinario[0], &codificado[0], nombre, nombredeco);
 
@@ -49,7 +64,7 @@ int main()
                 cout<<"El archivo texto de la clave decodificada no se pudo abrir."<<endl;
                 exit(1); }
 
-            while (getline(in, linea))
+            while (getline(in, linea))                                           //clave le lleva lo que hay en el archivo
                 clave += linea;
 
             in.close();
@@ -57,31 +72,152 @@ int main()
             cout<<"Ingrese la clave de administrador: "; cin>>claveusu;
 
             if(claveusu == clave){
-                cout<<"Administradro en linea."<<endl;
-                cout<<"Ingrese el nombre del archivo de los usuarios: "; cin>>archivoleer;
+                //if(metodo==1) deco1(tamaño1/8, semilla, &texbinario1[0], archivousuarios, archivousuarios);
+                //else deco2(tamaño1/8, semilla, &texbinario1[0], &codificado1[0], archivousuarios, archivousuarios);
 
-                on.open(archivoleer,ios::app);
+                cout<<"Administradro en linea."<<endl;
+
+                on.open(archivousuarios,ios::app);                                           //Abre el archivo de usuarios en modo escritura
                 if(on.fail()){
                     cout<<"El archivo texto de los usuarios no se pudo abrir."<<endl;
                     exit(1); }
 
                 cout<<"Ingrese el numero de usuarios que desea registrar: "; cin>>numerousu;
 
-                while(numerousu!=0){
-                    cout<<"Ingrese los datos del usuario nuevo cedula,clave,dinero: "; cin>>datos;
+                while(numerousu!=0){                                                          //ciclo para agregar usuarios
+                    cout<<"Ingrese los datos del usuario nuevo en modo: cedula,clave,dinero  "; cin>>datos;
                     on<<datos<<endl;
                     numerousu--;
                 }
                 on.close();
 
+                tamaño=tamtex(archivousuarios);
+                char catexto[tamaño];
+                int texbinario1[8*tamaño];
+                int codificado1[8*tamaño];
+
+                binario(tamaño, &catexto[0], &texbinario1[0], archivousuarios);
+
+                //if(metodo==1) metodo1(tamaño, semilla, &texbinario1[0], &codificado1[0], archivousuarios);
+                //else metodo2(tamaño, semilla, &texbinario1[0], &codificado1[0], archivousuarios);
+
             }
             else cout<<"Las claves no coinciden, no puede ingresar"<<endl;
 
+            clave="";
         }
 
+        else if(opcion==2){
+            cout<<"Para consultar saldo ingrese su cedula: "; cin>>cedula_ingre;
 
+            in.open(archivousuarios);
+            if(in.fail()){
+                cout<<"El archivo texto de los usuarios para consultar saldo no se pudo abrir."<<endl;
+                exit(1); }
 
+            while (getline(in, linea)) {
+                stringstream ss(linea);                                 //crea una linea de copia para facilitar extraer datos
 
+                getline(ss, cedula, ',');          // Extrae los componentes de la linea searados por la ,
+                getline(ss, nombre1, ',');
+                getline(ss, dinero_str, ',');
+
+                Usuario usuario = {cedula, nombre1, stoi(dinero_str)};     //comvierte el dinero en entero
+                usuarios.push_back(usuario);                               //agrega el usuario de la linea al vector de usuarios
+            }
+            in.close();
+
+            for (Usuario& usuario : usuarios) {                            //recorre el vector de usuarios y se planta en cada usuario
+                if (usuario.cedula == cedula_ingre) {
+                    cedulaEncontrada = true;                               //bandera para saber que el usuario esta
+
+                    if(usuario.dinero<1000)
+                        cout<<"Su dinero es insuficiente para colsultar saldo."<<endl;
+                    else {
+                        nuevodinero = usuario.dinero - 1000;
+                        cout << "Dinero actual: "<<nuevodinero;
+                        usuario.dinero = nuevodinero;     }
+
+                    break;
+                }
+            }
+
+            if (cedulaEncontrada) {
+
+                on.open(archivousuarios);                                    //Abre el archivo de usuarios en modo escritura
+                if(on.fail()){
+                    cout<<"El archivo texto de los usuarios no se pudo abrir para actualizar saldo."<<endl;
+                    exit(1); }
+
+                for (Usuario& usuario : usuarios)                           // Se escribe en el archivo el contenido del vector
+                    on << usuario.cedula << "," << usuario.nombre << "," << usuario.dinero << "\n";
+                on.close();
+
+            } else
+                cout << "El usuario no esta registrado." << endl;
+
+            usuarios.clear();
+
+        }
+
+        else if(opcion==3){
+            cout<<"Para consultar retirar dinero ingrese su cedula: "; cin>>cedula_ingre;
+            cout<<"Ingrese la cantidad de dinero a retirar: "; cin>>dineroretirar;
+
+            in.open(archivousuarios);
+            if(in.fail()){
+                cout<<"El archivo texto de los usuarios para retirar dinero no se pudo abrir."<<endl;
+                exit(1); }
+
+            while (getline(in, linea)) {
+                stringstream ss(linea);                                 //crea una linea de copia para facilitar extraer datos
+
+                getline(ss, cedula, ',');          // Extrae los componentes de la linea searados por la ,
+                getline(ss, nombre1, ',');
+                getline(ss, dinero_str, ',');
+
+                Usuario usuario = {cedula, nombre1, stoi(dinero_str)};     //comvierte el dinero en entero
+                usuarios.push_back(usuario);                               //agrega el usuario de la linea al vector de usuarios
+            }
+            in.close();
+
+            for (Usuario& usuario : usuarios) {                            //recorre el vector de usuarios y se planta en cada usuario
+                if (usuario.cedula == cedula_ingre) {
+                    cedulaEncontrada = true;                               //bandera para saber que el usuario esta
+
+                    if(usuario.dinero< dineroretirar+1000)
+                        cout<<"No es posible retirar dinero por que su saldo es insuficiente."<<endl;
+                    else {
+                        cout<<"Se a retirado "<<dineroretirar<<" de su cuenta. "<<endl;
+                        nuevodinero = usuario.dinero -dineroretirar -1000;
+                        cout << "Dinero actual: "<<nuevodinero;
+                        usuario.dinero = nuevodinero;     }
+
+                    break;
+                }
+            }
+
+            if (cedulaEncontrada) {
+
+                on.open(archivousuarios);                                    //Abre el archivo de usuarios en modo escritura
+                if(on.fail()){
+                    cout<<"El archivo texto de los usuarios no se pudo abrir para actualizar saldo."<<endl;
+                    exit(1); }
+
+                for (Usuario& usuario : usuarios)                           // Se escribe en el archivo el contenido del vector
+                    on << usuario.cedula << "," << usuario.nombre << "," << usuario.dinero << "\n";
+                on.close();
+
+            } else
+                cout << "El usuario no esta registrado." << endl;
+
+            usuarios.clear();
+
+        }
+
+        else{
+            cout<<"El programa a finalizado"<<endl;
+            break;}
 
     }
 }
@@ -142,6 +278,26 @@ void binario(int tamaño, char *catexto, int *texbinario, string archivoleer){
 
     for(int i=0; i<8*(tamaño); i++)                             //Se llena el arreglo de binarios con el vector
         texbinario[i]=vect[i];
+}
+
+void binAcadena(int tamaño, int *texbinario, string archivoleer){
+    char cadena[tamaño];
+
+    ifstream archivo;
+    archivo.open(archivoleer);
+
+    cout<<endl;
+    if(archivo.fail()){
+        cout<<"El archivo texto en binario no ce pudo abrir en la funcion binAcadena"<<endl;
+        exit(1);
+    }
+
+    archivo.read(cadena, tamaño);                                    //Se agrega el texto al arreglo caracter por caracter
+    archivo.close();
+
+    for(int i=0; i<tamaño; i++){
+        texbinario[i] = (int)cadena[i]- 48;
+    }
 }
 
 void metodo1(int tamaño, int semilla, int *texbinario, int *codificado, string nombre){
